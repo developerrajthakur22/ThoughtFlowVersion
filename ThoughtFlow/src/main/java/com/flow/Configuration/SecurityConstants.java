e85b37722cd18c35a258af44bdd3cacc39cdd450
Raj Thakur <121308092+developerrajthakur22@users.noreply.github.com>
2023-10-11 17:48:00 +0530

    Delete ThoughtFlow directory

--- ThoughtFlow/src/main/java/com/flow/Configuration/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.flow.Configuration;
-
-public interface SecurityConstants {
-
-	public static final String JWT_KEY = "secretthoughtflowapplicationrajthakur";
-	public static final String JWT_HEADER = "Authorization";
-	
-}
